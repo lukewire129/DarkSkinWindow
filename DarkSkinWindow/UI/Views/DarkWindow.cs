38486190185619fe598c1207d5546bb014a0b0ac
lukewire <lukewire129@gmail.com>
2023-08-31 18:49:08 +0900

    Window Skin Update.

--- DarkSkinWindow/UI/Views/DarkWindow.cs
+++ DarkSkinWindow/UI/Views/DarkWindow.cs
@@ -9,17 +9,49 @@ namespace DarkSkinWindow.UI.Views
 {
     public class DarkWindow : JamesWindow
     {
+        // Using a DependencyProperty as the backing store for TitleContent.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleTemplateProperty = DependencyProperty.Register ("TitleTemplate", typeof (DataTemplate), typeof (DarkWindow), new PropertyMetadata (null));
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register ("Title", typeof (object), typeof (DarkWindow), new UIPropertyMetadata (null));
+        public DataTemplate TitleTemplate
+        {
+            get { return (DataTemplate)GetValue (TitleTemplateProperty); }
+            set { SetValue (TitleTemplateProperty, value); }
+        }
+        public new object Title
+        {
+            get => (object)GetValue (TitleTemplateProperty);
+            set => SetValue (TitleTemplateProperty, value);
+        }
+
         static DarkWindow()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(DarkWindow), new FrameworkPropertyMetadata(typeof(DarkWindow)));
         }
-
         public override void OnApplyTemplate()
         {
             if (GetTemplateChild ("PART_CloseButton") is Button btn)
             {
                 btn.Click += (s, e) => Close ();
             }
+            if (GetTemplateChild ("PART_MinButton") is Button minbtn)
+            {
+                minbtn.Click += (s, e) => this.WindowState = WindowState.Minimized;
+            }
+            //if (GetTemplateChild ("PART_MaxButton") is MaximizeButton maxbtn)
+            //{
+            //    maxbtn.Click += (s, e) =>
+            //    {
+            //        if (maxbtn.Maximize)
+            //        {
+            //            this.WindowState = WindowState.Normal;
+            //            maxbtn.Maximize = !maxbtn.Maximize;
+            //            return;
+            //        }
+            //        this.WindowState = WindowState.Maximized;
+            //        maxbtn.Maximize = !maxbtn.Maximize;
+            //    };
+            //}
             if (GetTemplateChild ("PART_DragBar") is DraggableBar bar)
             {
                 bar.MouseDown += WindowDragMove;
